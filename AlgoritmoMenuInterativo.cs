143a580399621073625375d90afe72958ad4fbb9
João Guilherme Dias de Queiroz <131197147+Teytey0008@users.noreply.github.com>
2024-01-04 22:48:57 -0300

    Create AlgoritmoMenuInterativo.cs
    Construindo um menu interativo com while, default e Enviroment.Exit(0);

---
+++ AlgoritmoMenuInterativo.cs
@@ -0,0 +1,86 @@
+namespace CSHARP.Models;
+using System;
+
+class Progam
+{
+   static void Main();
+   {
+
+           string opcao;
+           
+
+      
+
+             while(true)
+             {
+               Console.WriteLine("Digite sua opcao");
+               Console.WriteLine("1- Cadastrar Cliente");
+               Console.WriteLine("2- Buscar Cliente ");
+               Console.WriteLine("3- Apagar Cliente ");
+               Console.WriteLine("4- Encerrar ");
+
+
+               opcao= Console.ReadLine();
+               
+               switch(pcao)
+               {  
+                 case "1":
+                  Console.WriteLine("Cadastro de Cliente");
+                  break;
+                 
+                 
+                 case "2": 
+                  Console.WriteLine("Busca de Cliente");
+                  break;
+                
+                  case "3":
+                   Console.WriteLine(" Apagar  Cliente");
+                  break;
+                  
+                  case "4":
+                   Console.WriteLine("Encerar");
+                   Enviroment.Exit(0);
+                   break;
+
+                  default:
+                   Console.WriteLine("Opção Invalida");
+                   break;
+
+                  
+                   
+                   
+                  
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+               }
+              
+        
+      
+     
+   
+   
+   
+   
+   
+            } 
+   }  
+      
+      
+      
