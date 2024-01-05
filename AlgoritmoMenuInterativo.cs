b69193fa1650b8a978b3870cf3bf9a8f547fa7c1
João Guilherme Dias de Queiroz <131197147+Teytey0008@users.noreply.github.com>
2024-01-04 23:07:50 -0300

    Update AlgoritmoMenuInterativo.cs
    Algoritmo Menu interativo com while

--- AlgoritmoMenuInterativo.cs
+++ AlgoritmoMenuInterativo.cs
@@ -1,86 +1,51 @@
-namespace CSHARP.Models;
 using System;
 
-class Progam
+namespace CSHARP.Models
 {
-   static void Main();
-   {
+    class Program
+    {
+        static void Main()
+        {
+            string opcao;
 
-           string opcao;
-           
+            while (true)
+            {
+                Console.WriteLine("Digite sua opcao");
+                Console.WriteLine("1- Cadastrar Cliente");
+                Console.WriteLine("2- Buscar Cliente ");
+                Console.WriteLine("3- Apagar Cliente ");
+                Console.WriteLine("4- Encerrar ");
 
-      
+                opcao = Console.ReadLine();
 
-             while(true)
-             {
-               Console.WriteLine("Digite sua opcao");
-               Console.WriteLine("1- Cadastrar Cliente");
-               Console.WriteLine("2- Buscar Cliente ");
-               Console.WriteLine("3- Apagar Cliente ");
-               Console.WriteLine("4- Encerrar ");
+                switch (opcao)
+                {
+                    case "1":
+                        Console.WriteLine("Cadastro de Cliente");
+                        break;
 
+                    case "2":
+                        Console.WriteLine("Busca de Cliente");
+                        break;
 
-               opcao= Console.ReadLine();
-               
-               switch(pcao)
-               {  
-                 case "1":
-                  Console.WriteLine("Cadastro de Cliente");
-                  break;
-                 
-                 
-                 case "2": 
-                  Console.WriteLine("Busca de Cliente");
-                  break;
-                
-                  case "3":
-                   Console.WriteLine(" Apagar  Cliente");
-                  break;
-                  
-                  case "4":
-                   Console.WriteLine("Encerar");
-                   Enviroment.Exit(0);
-                   break;
+                    case "3":
+                        Console.WriteLine("Apagar Cliente");
+                        break;
 
-                  default:
-                   Console.WriteLine("Opção Invalida");
-                   break;
+                    case "4":
+                        Console.WriteLine("Encerrar");
+                        Environment.Exit(0);
+                        break;
 
-                  
-                   
-                   
-                  
-                
+                    default:
+                        Console.WriteLine("Opção Inválida");
+                        break;
+                }
+            }
+        }
+    }
+}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-               }
-              
-        
-      
-     
-   
-   
-   
-   
-   
-            } 
-   }  
-      
-      
-      
+         
